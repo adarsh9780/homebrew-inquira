83523d44f01089b8faf3cf73352b4e0ac871004f
Adarsh Maurya <adarshmaurya7@gmail.com>
2026-04-18 18:01:23 +0530

    release: v0.5.30

--- Casks/inquira.rb
+++ Casks/inquira.rb
@@ -1,8 +1,8 @@
 cask "inquira" do
-  version "0.5.29"
-  sha256 "5d66fbb964775d778a71d8e09d01bf2c80a5d74cdae1d58b9f29aff49af58ab1"
+  version "0.5.30"
+  sha256 "1e56be1410f561d14c9b06a0bb1cf8917b1cfcd62d19a501381b89e18a9dae38"
 
-  url "https://downloads.inquiraai.com/v0.5.29/Inquira_0.5.29_aarch64.dmg"
+  url "https://downloads.inquiraai.com/v0.5.30/Inquira_0.5.30_aarch64.dmg"
   name "Inquira"
   desc "Local-first AI data analysis desktop app"
   homepage "https://inquiraai.com"
